4debf585d8ee67ef4d73808bb4f3e16d21c22ff3
jitendradev <jitendra.panwar2002@gmail.com>
2021-05-24 11:37:00 +1000

    Terraform Module V0.1

---
+++ ec2/variable.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {
+  default = "t2.small"
+}
+
+variable "region" {
+  default = "ap-southeast-2"
+}
---
+++ sg/sg.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH into VPC"
+    from_port   = 23
+    to_port     = 23
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Outbound Allowed"
+    from_port   = 1024
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
